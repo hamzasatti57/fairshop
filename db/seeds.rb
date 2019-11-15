cbfa8bccbfa90b07c331c4bc163f17c5810dd05a
HamzaSohail <hamza.sohail@algorepublic.com>
2019-11-15 16:51:48 +0500

    New Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,8 @@ for i in arr do
 end
 
 Country.create(title: 'Pakistan')
-User.create(email:'admin@furnishia.com', password: 'admin@123', role: 0, first_name: 'Administrator',
-            last_name: '', username: 'admin', contact_details: 'Admin', category_id: 1, city_id: 1)
+User.create(email:'superadmin@furnishia.com', password: 'superadmin', role: 0, first_name: 'Super',
+            last_name: 'Admin', username: 'superadmin', contact_details: 'Admin', category_id: 1, city_id: 1)
 
 
 
