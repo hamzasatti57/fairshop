3473dff52cbebb109f25bfa055a3124723a99676
Hamza Satti <hamza.satti159@gmail.com>
2020-06-29 16:39:36 +0500

    Bug fix

--- app/controllers/product_category_controller.rb
+++ app/controllers/product_category_controller.rb
@@ -3,6 +3,16 @@ class ProductCategoryController < ApplicationController
     # @product_category = ProductCategory.find(params[:product_category_id])
     # @product_category.punch(request)
     # @products = Product.where(product_category_id: params[:product_category_id]).paginate(page: params[:page], per_page: 21)
-    @products = Product.all
+    if params[:type].present?
+      query = "%#{params[:type].gsub("_", " ")}%"
+      @caterogy = Category.where("title ILIKE ?", query).first
+      @products = Product.where(product_category_id: @caterogy.product_categories.pluck(:id))
+    elsif params[:brand].present?
+      query = "%#{params[:brand].gsub("_", " ")}%"
+      @brand = Company.where("title ILIKE ?", query).first
+      @products = Product.where(company_id: @brand.id) 
+    else
+      @products = Product.all
+    end
   end
 end
