23f0abb1ed3284f22ef733763d9d2d52b77cd37f
Hamza Satti <hamza.satti159@gmail.com>
2020-08-04 13:52:41 +0500

    add to cart validation fix

--- app/models/user_cart_product.rb
+++ app/models/user_cart_product.rb
@@ -2,6 +2,6 @@ class UserCartProduct < ApplicationRecord
   belongs_to :user
   belongs_to :product
   belongs_to :user_cart
-  validates_uniqueness_of :product, scope: :user_id, message: "Already added to cart."
+  validates_uniqueness_of :product, scope: [:user_id, :user_cart_id], message: "Already added to cart."
 
 end
