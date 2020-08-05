4909fc63091c893bca20b65e72a0a8ec036d2d9b
Hamza Satti <hamzah905>
2020-08-05 20:43:35 +0500

    Bug fix

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -31,28 +31,28 @@ class CheckoutController < ApplicationController
     @user = User.find_by_email(params["email_address"])
     @user.user_carts.last.update(status: 2)
     UserPayment.create!(user_id: @user.present? ? @user.id : current_user.id, amount: params["amount_gross"])
-    xml = File.open(Rails.root.join('public', 'Sales.xml'))
-    data = Hash.from_xml(xml)
-    _file_name = "Sale_Invoice_#{Time.now.strftime("%Y_%d_%m_%H_%M").to_s}"
-    data["Transaction"]["SalesHeader"]["CustomerName"] = params["name_first"]
-    data["Transaction"]["SalesHeader"]["TotalSalePriceAfterDiscount"] = params["amount_gross"]
-    data["Transaction"]["SalesHeader"]["DateOfSale"] = Time.now.to_s
-    sale_detail = {"StockItemId"=>"14CB7ADA-295E-43FD-AECD-243106D55445", "Quantity"=>"1", "UnitSellingPrice"=>"999.9900", "DiscountPerUnit"=>"0.0000", "UnitPriceAfterDiscount"=>"999.9900", "TotalPriceAfterDiscount"=>"999.9900", "UnitVAT"=>"130.4335"}
-    data["Transaction"]["Details"]["SalesDetails"] = []
-    detail_array = @user.user_carts.last.user_cart_products.each do |product|
-      sale_detail["Quantity"] = product.quantity
-      sale_detail["UnitSellingPrice"] = product.product.price
-      sale_detail["TotalPriceAfterDiscount"] = product.product.price
-    end
-    data["Transaction"]["Details"]["SalesDetails"] << detail_array.map(&:attributes)
-    data["Transaction"]["Details"]["SalesDetails"] = data["Transaction"]["Details"]["SalesDetails"].flatten
-    data["Transaction"]["DeliveryDetails"]["Province"] = @user.user_carts.last.checkout.billing_address.province.title
-    data["Transaction"]["DeliveryDetails"]["City"] = @user.user_carts.last.checkout.billing_address.city.title
-    data["Transaction"]["DeliveryDetails"]["Address"] = @user.user_carts.last.checkout.billing_address.address
-    data["Transaction"]["DeliveryDetails"]["PostalCode"] = @user.user_carts.last.checkout.billing_address.postal_code
-    puts data.to_xml
-    FileUtils.rm_rf(Rails.root.join('public/Sales/', "#{_file_name}.xml"))
-    File.open("#{Rails.root}/public/Sales/#{_file_name}.xml", "w+b") << data.to_xml
+    # xml = File.open(Rails.root.join('public', 'Sales.xml'))
+    # data = Hash.from_xml(xml)
+    # _file_name = "Sale_Invoice_#{Time.now.strftime("%Y_%d_%m_%H_%M").to_s}"
+    # data["Transaction"]["SalesHeader"]["CustomerName"] = params["name_first"]
+    # data["Transaction"]["SalesHeader"]["TotalSalePriceAfterDiscount"] = params["amount_gross"]
+    # data["Transaction"]["SalesHeader"]["DateOfSale"] = Time.now.to_s
+    # sale_detail = {"StockItemId"=>"14CB7ADA-295E-43FD-AECD-243106D55445", "Quantity"=>"1", "UnitSellingPrice"=>"999.9900", "DiscountPerUnit"=>"0.0000", "UnitPriceAfterDiscount"=>"999.9900", "TotalPriceAfterDiscount"=>"999.9900", "UnitVAT"=>"130.4335"}
+    # data["Transaction"]["Details"]["SalesDetails"] = []
+    # detail_array = @user.user_carts.last.user_cart_products.each do |product|
+    #   sale_detail["Quantity"] = product.quantity
+    #   sale_detail["UnitSellingPrice"] = product.product.price
+    #   sale_detail["TotalPriceAfterDiscount"] = product.product.price
+    # end
+    # data["Transaction"]["Details"]["SalesDetails"] << detail_array.map(&:attributes)
+    # data["Transaction"]["Details"]["SalesDetails"] = data["Transaction"]["Details"]["SalesDetails"].flatten
+    # data["Transaction"]["DeliveryDetails"]["Province"] = @user.user_carts.last.checkout.billing_address.province.title
+    # data["Transaction"]["DeliveryDetails"]["City"] = @user.user_carts.last.checkout.billing_address.city.title
+    # data["Transaction"]["DeliveryDetails"]["Address"] = @user.user_carts.last.checkout.billing_address.address
+    # data["Transaction"]["DeliveryDetails"]["PostalCode"] = @user.user_carts.last.checkout.billing_address.postal_code
+    # puts data.to_xml
+    # FileUtils.rm_rf(Rails.root.join('public/Sales/', "#{_file_name}.xml"))
+    # File.open("#{Rails.root}/public/Sales/#{_file_name}.xml", "w+b") << data.to_xml
   end
 
   def cancel_payment
