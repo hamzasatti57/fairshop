742207f1a45269452bb78dda88c7b749c2e2e1b6
Hamza Satti <hamzah905>
2020-08-17 17:07:40 +0500

    Bug fix

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,7 +2,7 @@ class CartController < ApplicationController
 
 
   def index
-    @cart = current_user.user_carts.where(status: 0).last.user_cart_products if current_user.user_carts.where(status: 0).present?
+    @cart = current_user.user_carts.where(status: 0).last.user_cart_products.order("created_at DESC") if current_user.user_carts.where(status: 0).present?
     @sum = current_user.user_carts.where(status: 0).last.user_cart_products.pluck(:sub_total).sum if current_user.user_carts.where(status: 0).present? && current_user.user_carts.where(status: 0).last.user_cart_products.present?
   end
 
