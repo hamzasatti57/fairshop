790635a06f57925358ff4448df2113491a66d4f2
Hamza Satti <hamzah905>
2020-08-18 16:23:08 +0500

    Added facebook share logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def selected_category_type(form)
     Category.category_types[form.object.category_type]
   end
+
+  def product_attributes product
+    { id: product.id, title: product.title, file_name: rails_blob_url(product.images.first) }.to_json
+  end
 end
