6bbf5f83166fd4b676d386b103fff2a9be5acd4b
Hamza Satti <hamzah905>
2020-09-17 14:37:10 +0500

    Bug fix

--- app/controllers/confirmation_controller.rb
+++ app/controllers/confirmation_controller.rb
@@ -23,7 +23,7 @@ class ConfirmationController < ApplicationController
       data["Transaction"]["SalesHeader"]["TotalSalePriceAfterDiscount"] = @sum.to_s
       data["Transaction"]["SalesHeader"]["OtpCode"] = random_number.to_s
       data["Transaction"]["SalesHeader"]["DateOfSale"] = Time.now.to_s
-      data["Transaction"]["Details"] = []
+      data["Transaction"]["Details"]["SalesDetails"] = []
       sale_details = {"StockItemId"=>"14CB7ADA-295E-43FD-AECD-243106D55445", "Quantity"=>"1", "UnitSellingPrice"=>"999.9900", "DiscountPerUnit"=>"0.0000", "UnitPriceAfterDiscount"=>"999.9900", "TotalPriceAfterDiscount"=>"999.9900", "UnitVAT"=>"130.4335"}
       current_user.user_carts.last.user_cart_products.each do |product|
         sale_details["Quantity"] = product.quantity.to_s
@@ -32,7 +32,7 @@ class ConfirmationController < ApplicationController
         sale_details["TotalPriceAfterDiscount"] = product.product.price.to_s
         sale_details["UnitPriceAfterDiscount"] = product.product.price.to_s
         sale_details["UnitVAT"] = (product.product.price.to_i * 0.15).to_s
-        data["Transaction"]["Details"] << sale_details
+        data["Transaction"]["Details"]["SalesDetails"] << sale_details
       end
       # data["Transaction"]["Details"]["SalesDetails"] = data["Transaction"]["Details"]["SalesDetails"].flatten
       data["Transaction"]["DeliveryDetails"]["Province"] = current_user.user_carts.last.checkout.billing_address.province.title
