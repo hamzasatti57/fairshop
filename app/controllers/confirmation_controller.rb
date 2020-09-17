8286efdff7d8cf471de1b6250bfb23c148556d32
Hamza Satti <hamzah905>
2020-09-17 14:35:31 +0500

    Bug fix

--- app/controllers/confirmation_controller.rb
+++ app/controllers/confirmation_controller.rb
@@ -24,15 +24,15 @@ class ConfirmationController < ApplicationController
       data["Transaction"]["SalesHeader"]["OtpCode"] = random_number.to_s
       data["Transaction"]["SalesHeader"]["DateOfSale"] = Time.now.to_s
       data["Transaction"]["Details"] = []
-      SalesDetails = {"StockItemId"=>"14CB7ADA-295E-43FD-AECD-243106D55445", "Quantity"=>"1", "UnitSellingPrice"=>"999.9900", "DiscountPerUnit"=>"0.0000", "UnitPriceAfterDiscount"=>"999.9900", "TotalPriceAfterDiscount"=>"999.9900", "UnitVAT"=>"130.4335"}
+      sale_details = {"StockItemId"=>"14CB7ADA-295E-43FD-AECD-243106D55445", "Quantity"=>"1", "UnitSellingPrice"=>"999.9900", "DiscountPerUnit"=>"0.0000", "UnitPriceAfterDiscount"=>"999.9900", "TotalPriceAfterDiscount"=>"999.9900", "UnitVAT"=>"130.4335"}
       current_user.user_carts.last.user_cart_products.each do |product|
-        SalesDetails["Quantity"] = product.quantity.to_s
-        SalesDetails["UnitSellingPrice"] = product.product.price.to_s
-        SalesDetails["StockItemID"] = product.product.stock_item_id.to_s
-        SalesDetails["TotalPriceAfterDiscount"] = product.product.price.to_s
-        SalesDetails["UnitPriceAfterDiscount"] = product.product.price.to_s
-        SalesDetails["UnitVAT"] = (product.product.price.to_i * 0.15).to_s
-        data["Transaction"]["Details"] << SalesDetails
+        sale_details["Quantity"] = product.quantity.to_s
+        sale_details["UnitSellingPrice"] = product.product.price.to_s
+        sale_details["StockItemID"] = product.product.stock_item_id.to_s
+        sale_details["TotalPriceAfterDiscount"] = product.product.price.to_s
+        sale_details["UnitPriceAfterDiscount"] = product.product.price.to_s
+        sale_details["UnitVAT"] = (product.product.price.to_i * 0.15).to_s
+        data["Transaction"]["Details"] << sale_details
       end
       # data["Transaction"]["Details"]["SalesDetails"] = data["Transaction"]["Details"]["SalesDetails"].flatten
       data["Transaction"]["DeliveryDetails"]["Province"] = current_user.user_carts.last.checkout.billing_address.province.title
