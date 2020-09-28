9f60dec589c3f7f44d8eaa2da4ceaf9618959806
Hamza Satti <hamzah905>
2020-09-28 19:25:21 +0500

    Bug fix

--- app/controllers/confirmation_controller.rb
+++ app/controllers/confirmation_controller.rb
@@ -51,7 +51,7 @@ class ConfirmationController < ApplicationController
       data["Transaction"]["DeliveryDetails"]["CustomerName"] = current_user.first_name + " " + current_user.last_name
       data["Transaction"]["DeliveryDetails"]["DeliveryPrice"] = (@sum.to_i * 0.10).to_s
       data["Transaction"]["DeliveryDetails"]["Instructions"] = current_user.user_carts.last.checkout.billing_address.instruction if current_user.user_carts.last.checkout.present?
-      data["Transaction"]["SalesHeader"]["UnitNo"] = (current_user.user_carts.last.id + 1000).to_s
+      # data["Transaction"]["SalesHeader"]["UnitNo"] = (current_user.user_carts.last.id + 1000).to_s
       data["Transaction"]["Details"] = data["Transaction"]["Details"]["SalesDetails"]
       data = data.to_xml.to_s.gsub("Detail>", "SalesDetails>").gsub(" type=\"array\"", "").to_yaml
       logger.info "=========#{data}=========="
