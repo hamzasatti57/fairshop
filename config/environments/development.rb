6937575d60604efd3691e8c10de74ba9ed6ac216
Hamza Satti <hamzah905>
2020-10-05 14:37:33 +0500

    Bug fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -86,10 +86,10 @@ Rails.application.configure do
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-      :address              => 'smtp.gmail.com',
-      :port                 => 587,
-      :user_name            => 'hamza.satti@algorepublic.com',
-      :password             => 'H@mza321',
+      :address              => 'mail.fairprice.co.za',
+      :port                 => 25,
+      :user_name            => 'onlineorders',
+      :password             => 'Password001',
       :authentication       => 'plain',
       :enable_starttls_auto => true
   }
