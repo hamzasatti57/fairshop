a47be0e418183d1bc62cbad5916bc941e40bbae0
Hamza Satti <hamzah905>
2020-10-08 19:30:46 +0500

    Bug fix

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -9,9 +9,11 @@ class ContactUsController < ApplicationController
     @stores.each_with_index do |store, index|
       next if store.lat == nil or store.long == nil
       @lat_long[index] = []
-      @lat_long[index] << store.store_name
-      @lat_long[index] << store.lat
-      @lat_long[index] << store.long
+      @lat_long[index] << store.store_name.to_s
+      @lat_long[index] << store.lat.to_f
+      @lat_long[index] << store.long.to_f
+      @lat_long << @lat_long[index]
     end
+    @lat_long = @lat_long.compact
   end
 end
