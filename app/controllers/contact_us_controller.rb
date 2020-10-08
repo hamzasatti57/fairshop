6e59209e88f74adc71d3b69e0c1630aa2e2ba0cb
Hamza Satti <hamzah905>
2020-10-08 18:42:33 +0500

    Map fix

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -4,6 +4,14 @@ class ContactUsController < ApplicationController
   end
 
   def store_locator
-
+    @stores = Store.all
+    @lat_long = []
+    @stores.each_with_index do |store, index|
+      next if store.lat == nil or store.long == nil
+      @lat_long[index] = []
+      @lat_long[index] << store.store_name
+      @lat_long[index] << store.lat
+      @lat_long[index] << store.long
+    end
   end
 end
