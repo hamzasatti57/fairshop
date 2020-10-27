4261e4b2e6e4ddbcbeefecf19e3855e61f580844
--global <hamzah905>
2020-10-27 17:46:56 +0500

    Bug fix

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -7,20 +7,22 @@ class ContactUsController < ApplicationController
     if params[:search].present?
       @stores = Store.where("store_address ILIKE '%#{params[:search].downcase}%' OR store_city ILIKE '%#{params[:search].downcase}%'")
       @store_cards = @stores
-    else
-      # @stores = Store.all
     end
-
-    # @store_info = @stores.last
-    @lat_long = []
-    @stores.to_a.each_with_index do |store, index|
-      next if store.lat == nil or store.long == nil
-      @lat_long[index] = []
-      @lat_long[index] << store.store_name.to_s
-      @lat_long[index] << store.lat.to_f
-      @lat_long[index] << store.long.to_f
-      @lat_long << @lat_long[index]
+    if params["store_item"].present?
+      @store = Store.find_by_store_name(params["store_item"])
+      @lat_long = []
+      # @stores.to_a.each_with_index do |store, index|
+        # next if store.lat == nil or store.long == nil
+        # @lat_long_1 = []
+        @lat_long << @store.store_name.to_s
+        @lat_long << @store.lat.to_f
+        @lat_long << @store.long.to_f
+        # @lat_long << @lat_long_1
+      # end
+      @lat_long = @lat_long.compact
+      if request.xhr? == 0
+        render partial: "store_info"
+      end
     end
-    @lat_long = @lat_long.compact
   end
 end
