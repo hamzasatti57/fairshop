d1814e3e1e3a3e275fec3d9a6be879cbad9d4ec5
Hamza Satti <hamzah905>
2020-11-06 14:44:41 +0500

    Bug fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,13 @@
 class UserMailer < ApplicationMailer
   default from: 'onlineorders@fairprice.co.za'
 
-  def order_confiramtion_email(user, checkout, billing_address, cart, sum)
+  def order_confiramtion_email(user, checkout, billing_address, cart, sum, shipping_price)
     @user = user
     @checkout = checkout
     @billing_address = billing_address
     @cart = cart
     @sum = sum
+    @shipping_price = shipping_price
     mail(to: @user.email, subject: "Thank you for shopping here")
   end
 
