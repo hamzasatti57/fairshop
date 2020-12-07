bd2af00b05fabad1703386ee5248bb57a33586a8
Hamza Satti <hamzah905>
2020-12-07 13:20:48 +0500

    Base Url fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,9 +111,9 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { :host => 'http://13.244.178.144/' }
+  config.action_mailer.default_url_options = { :host => 'http://test.fairprice.co.za/' }
 
-  config.action_mailer.asset_host = { :host => 'http://13.244.178.144/' }
+  config.action_mailer.asset_host = { :host => 'http://test.fairprice.co.za/' }
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
