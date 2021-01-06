0aeecd372450f8b7cf04fe73f759e28529c3b010
Hamza Satti <hamzah905>
2021-01-06 20:13:15 +0500

    Bug fix

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Furnishia
   class Application < Rails::Application
+
+    
+    config.time_zone = 'Africa/Johannesburg'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
